345c312aefe50deacc1fff5473841def2de72891
kajal <kajal.agr0706@gmail.com>
2024-02-21 20:42:52 +0530

    Calculator

---
+++ DevSoc/android/app/src/main/java/com/example/devsoc/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.devsoc;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ DevSoc/android/app/src/main/java/com/example/devsoc/practicefile.1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int length = 4, breadth = 5;
+    printf("Lenght:%d, Breadth:%d,",length, breadth,);
+    printf("Area:%d\n",length*breadth);
+    return 0;
+}
